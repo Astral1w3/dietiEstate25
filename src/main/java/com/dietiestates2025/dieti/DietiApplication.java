adc2ef228b49d18cc3ae4264a9bee5ce50376007
Astral1w3 <angelomarcone4@gmail.com>
2025-02-17 13:39:01 +0100

    Uploading All Files

---
+++ src/main/java/com/dietiestates2025/dieti/DietiApplication.java
@@ -0,0 +1,13 @@
+package com.dietiestates2025.dieti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dietiestates2025/dieti/DietiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dietiestates2025.dieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
